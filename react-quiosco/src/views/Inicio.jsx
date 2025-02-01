bb48c648718b4af4cd64ba55348780ad32eb372d
GeDaCai97 <car.ar.16@hotmail.com>
2025-02-01 11:47:19 -0700

    Avance del main layout

--- react-quiosco/src/views/Inicio.jsx
+++ react-quiosco/src/views/Inicio.jsx
@@ -1,5 +1,22 @@
+import {productos} from '../data/productos';
+import Producto from '../components/Producto'
+import useQuiosco from '../hooks/useQuiosco';
 export default function Inicio() {
+  const { categoriaActual } = useQuiosco();
   return (
-    <div>Inicio</div>
+    <>
+      <h1 className='text-4xl font-black'>{ categoriaActual.nombre }</h1>
+      <p className='text-2xl my-10'>
+        Elige y personaliza tu pedido a continuación
+      </p>
+      <div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3'>
+        {productos.map( producto => (
+          <Producto
+            key={producto.imagen}
+            producto={producto}
+          />
+        ))}
+      </div>
+    </>
   )
 }
